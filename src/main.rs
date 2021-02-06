e3e7f7c45b087bec3bec7675939003a4f160a1d5
PryosCode <info@pryoscode.net>
2021-02-06 17:56:21 +0100

    Add clap support

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+extern crate clap;
+use clap::{App, Arg};
+
 mod lexer;
 mod parser;
 
 fn main() {
-    if std::env::args().len() > 1 {
-        let arg = std::env::args().nth(1).unwrap();
-        let string = std::fs::read_to_string(arg).expect("Error");
-        println!("{}", string);
-    }
+    let matches = App::new("Cinyras")
+        .version("0.0.1")
+        .author("PryosCode <info@pryoscode.net>")
+        .about("Cinyras is a high level general purpose interpreted scripting language with its own interpreter written in Rust.")
+        .arg(Arg::with_name("INPUT")
+            .help("Sets the input file to use")
+            .required(true))
+        .get_matches();
+
+    let file = matches.value_of("INPUT").unwrap();
+    let string = std::fs::read_to_string(file).expect("Error");
+    println!("{}", string);
 }
